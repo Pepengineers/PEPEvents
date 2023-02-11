0659b2f36b612a60080df42af0ebf459ace1e196
Tesstarosa <raeinforce@gmail.com>
2023-02-12 00:46:53 +0300

    add Raise methods

--- Runtime/GameEvent.cs
+++ Runtime/GameEvent.cs
@@ -1,10 +1,16 @@
-﻿using PEPEngineers.PEPEvents.Interface;
+﻿using PEPEngineers.PEPEvents.Extensions;
+using PEPEngineers.PEPEvents.Interface;
 using UnityEngine;
 
 namespace PEPEngineers.PEPEvents.Runtime
 {
-	public abstract class GameEvent : ScriptableObject, IBroker
+	[CreateAssetMenu(menuName = "Events/" + nameof(GameEvent), fileName = nameof(GameEvent), order = 0)]
+	public class GameEvent : ScriptableObject, IBroker
 	{
+		public void Raise<T>(T message) where T : struct, IMessage
+		{
+			this.Publish(message);
+		}
 	}
 
 	public abstract class GameEvent<TMessage> : GameEvent, IBroker<TMessage>
